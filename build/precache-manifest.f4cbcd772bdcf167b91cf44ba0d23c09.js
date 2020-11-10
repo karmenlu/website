553e0767f0f0e91a72bf3c6bbe46abca42aa4676
Karmen Lu <karmen@loseit.com>
2020-11-10 15:15:29 -0500

    Move all files up a directory.

---
+++ build/precache-manifest.f4cbcd772bdcf167b91cf44ba0d23c09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6f2dcfe361bd1ba227ac6c5c01c27f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fd6bdbf92822821d470",
+    "url": "/static/css/main.37f6cc80.chunk.css"
+  },
+  {
+    "revision": "01df5acd4b87d7aa4b79",
+    "url": "/static/js/2.f93d8254.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.f93d8254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd6bdbf92822821d470",
+    "url": "/static/js/main.90411186.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  }
+]);
