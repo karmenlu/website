9de719032cba245451f0200b17aef94c5f4e03a1
Karmen Lu <karmen@loseit.com>
2020-11-15 15:59:51 -0500

    Add welcome section. Update Experience, Projects, About and Connect sections. Add links to projects and external URLs. Style navigation menu. Style with black and white color scheme.

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/images', express.static(path.join(__dirname, '/public/images/')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +19,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use(express.static('public'));
 app.use('/favicon.ico', express.static(__dirname + '/public/images/favicon.ico'));
 app.use('/bootstrap/css/', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
