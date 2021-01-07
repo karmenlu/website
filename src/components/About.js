12585c691d002df2acf76b911a0a183295bbc078
Karmen Lu <lukarm3n@gmail.com>
2021-01-07 18:39:14 -0500

    Edit About section.

--- src/components/About.js
+++ src/components/About.js
@@ -8,22 +8,19 @@ class About extends Component {
                 <div className="content row align-self-center">
                     <div className="sectionBodyText">
                         I am self-motivated and resilient. As an undergraduate student, I have leveraged opportunities
-                        to collaborate with peers, to ask questions, and to learn from everyone around me. Since
-                        entering
-                        college as a first generation student with no prior programming experience, I have been growing
-                        as
-                        an engineer through coursework, involvement in student organizations, and technical
-                        work experiences. Through technical internships in frontend, backend, and infrastructure
-                        roles, I learned about the software industry and about how to work in large codebases.
+                        to collaborate with peers, to ask questions, and to learn from everyone around me. Although
+                        I entered college as a first generation student with no prior programming experience, I have grown
+                        as an engineer through coursework, involvement in student organizations, and technical
+                        work experiences. Working as frontend, backend, and infrastructure roles, I learned about 
+                        the software industry and about how to work in large codebases. 
                         And now, I continue to learn. Committed to personal progress and curious about new technologies,
-                        I know that
-                        my journey is just beginning.
+                        I know that my journey is just beginning.
                         <br/><br/>
                         With graduation around the corner (May 2021), I am seeking full-time opportunities. While most
                         of my past
                         roles have centered around web development, I am an enthusiastic learner and would be open to
                         any opportunities in
-                        software engineering/development. In particular, I am interested in the health and wellness
+                        software engineering/development. In particular, I am interested in health and wellness
                         technology.
                         <br/><br/>
                         Outside of work and school, my other interests include road cycling, running, inline skating,
